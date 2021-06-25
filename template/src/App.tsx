02bf64612b4c8dca87b121ba9dd734651323c5ac
leesuhyung <realnavystyle@gmail.com>
2021-06-25 16:30:32 +0900

    prettier auto fix test

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -7,8 +7,7 @@ const App = (): ReactElement => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload..
+        <p>Edit <code>src/App.tsx</code> and save to reload..
         </p>
         <a
           className="App-link"
